36c1938f6db18f6619e5fd63c4919ebee577187e
leangaurav <leangaurav.me@gmail.com>
2023-11-17 19:42:52 +0530

    Google search resutls (#1)
    * Google search resutls
    
    * Call getResult function
    
    * change return to 0

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main() {
-    cout << "hello" << scrapeWebPage();
+    cout << "hello" << scrapeWebPage() << fetchGoogleSearchResult();
     return 0;
 }
 
 string scrapeWebPage() {
     return  "dummy";
+}
+int fetchGoogleSearchResult() {
+    return 0;
 }
